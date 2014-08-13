65ab92352b4936c3bf8827533f8ffd7b6b8ae7e1
István Donkó <istvan.donko+github@gmail.com>
2014-08-13 14:52:49 +0200

    Card detection, and measurement

--- SetBot/SetBot/MainForm.Designer.cs
+++ SetBot/SetBot/MainForm.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.setAreaButton = new System.Windows.Forms.Button();
             this.captureButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.findCardsButton = new System.Windows.Forms.Button();
+            this.saveImageButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // setAreaButton
@@ -44,7 +48,7 @@
             // 
             // captureButton
             // 
-            this.captureButton.Location = new System.Drawing.Point(107, 13);
+            this.captureButton.Location = new System.Drawing.Point(94, 13);
             this.captureButton.Name = "captureButton";
             this.captureButton.Size = new System.Drawing.Size(75, 23);
             this.captureButton.TabIndex = 1;
@@ -52,16 +56,61 @@
             this.captureButton.UseVisualStyleBackColor = true;
             this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
             // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(13, 57);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(37, 13);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "output";
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.Location = new System.Drawing.Point(266, 13);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(75, 23);
+            this.analyzeButton.TabIndex = 3;
+            this.analyzeButton.Text = "Analyze";
+            this.analyzeButton.UseVisualStyleBackColor = true;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // findCardsButton
+            // 
+            this.findCardsButton.Location = new System.Drawing.Point(176, 13);
+            this.findCardsButton.Name = "findCardsButton";
+            this.findCardsButton.Size = new System.Drawing.Size(75, 23);
+            this.findCardsButton.TabIndex = 2;
+            this.findCardsButton.Text = "Find Cards";
+            this.findCardsButton.UseVisualStyleBackColor = true;
+            this.findCardsButton.Click += new System.EventHandler(this.findCardsButton_Click);
+            // 
+            // saveImageButton
+            // 
+            this.saveImageButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveImageButton.Location = new System.Drawing.Point(266, 272);
+            this.saveImageButton.Name = "saveImageButton";
+            this.saveImageButton.Size = new System.Drawing.Size(75, 23);
+            this.saveImageButton.TabIndex = 5;
+            this.saveImageButton.Text = "Save Image";
+            this.saveImageButton.UseVisualStyleBackColor = true;
+            this.saveImageButton.Click += new System.EventHandler(this.saveImageButton_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(353, 307);
+            this.Controls.Add(this.saveImageButton);
+            this.Controls.Add(this.findCardsButton);
+            this.Controls.Add(this.analyzeButton);
+            this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.captureButton);
             this.Controls.Add(this.setAreaButton);
             this.Name = "MainForm";
             this.Text = "SetBot";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +118,10 @@
 
         private System.Windows.Forms.Button setAreaButton;
         private System.Windows.Forms.Button captureButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button analyzeButton;
+        private System.Windows.Forms.Button findCardsButton;
+        private System.Windows.Forms.Button saveImageButton;
     }
 }
 
--- SetBot/SetBot/MainForm.cs
+++ SetBot/SetBot/MainForm.cs
@@ -13,8 +13,13 @@ namespace SetBot
     public partial class MainForm : Form
     {
         public bool settingArea = false;
-
         public Rectangle area = new Rectangle();
+
+        Bitmap bmp;
+
+        public Point cardStart;
+        public Size cardSize;
+        public Size gapSize;
 
         public MainForm()
         {
@@ -29,21 +34,122 @@ namespace SetBot
                 area.Y = Cursor.Position.Y;
 
                 settingArea = true;
+                setAreaButton.BackColor = SystemColors.ControlDark;
             }
 
             else
             {
                 area.Width = Cursor.Position.X - area.X;
                 area.Height = Cursor.Position.Y - area.Y;
+
+                settingArea = false;
+                setAreaButton.BackColor = SystemColors.Control;
             }
         }
 
         private void captureButton_Click(object sender, EventArgs e)
         {
-            Bitmap bmp = new Bitmap(area.Width, area.Height);
+            bmp = new Bitmap(area.Width, area.Height);
             Graphics graphics = Graphics.FromImage(bmp);
-            graphics.CopyFromScreen(area.X, area.Y, 0, 0, area.Size);
+            graphics.CopyFromScreen(area.X, area.Y, 0, 0, area.Size);            
+        }
 
+        private void findCardsButton_Click(object sender, EventArgs e)
+        {
+            int currentX, currentY;
+
+            #region X axis
+
+            currentX = 0;
+            currentY = 50;
+
+            while (!isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentX++;
+            }
+
+            cardStart.X = currentX;
+
+            while (isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentY--;
+            }
+            currentY++;
+
+            while (isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentX++;
+            }
+
+            cardSize.Width = currentX - cardStart.X;
+
+            while (!isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentX++;
+            }
+
+            gapSize.Width = currentX - (cardStart.X + cardSize.Width);
+
+            #endregion
+
+            #region Y axis
+
+            currentX = 50;
+            currentY = 0;
+
+            while (!isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentY++;
+            }
+
+            cardStart.Y = currentY;
+
+            while (isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentX--;
+            }
+            currentX++;
+
+            while (isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentY++;
+            }
+
+            cardSize.Height = currentY - cardStart.Y;
+
+            while (!isWhite(bmp.GetPixel(currentX, currentY)))
+            {
+                currentY++;
+            }
+
+            gapSize.Height = currentY - (cardStart.Y + cardSize.Height);
+
+            #endregion
+            MessageBox.Show("X:" + cardStart.X + "|Y:" + cardStart.Y);
+            MessageBox.Show("X:" + cardSize.Width + "|Y:" + cardSize.Height);
+            MessageBox.Show("X:" + gapSize.Width + "|Y:" + gapSize.Height);
+        }
+
+        private void analyzeButton_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = Graphics.FromImage(bmp);
+
+            for (int i = 0; i < 4*4; i++)
+            {
+                graphics.DrawRectangle(Pens.Black,
+                    cardStart.X + (cardSize.Width + gapSize.Width) * (i % 4),
+                    cardStart.Y + (cardSize.Height + gapSize.Height) * (i / 4),
+                    cardSize.Width, cardSize.Height);
+            }
+        }
+
+        public bool isWhite(Color color)
+        {
+            return color.R > 200 && color.G > 200 && color.B > 200;
+        }
+
+        private void saveImageButton_Click(object sender, EventArgs e)
+        {
             bmp.Save(@"C:\Users\isti\Desktop\test.png", System.Drawing.Imaging.ImageFormat.Png);
         }
     }
