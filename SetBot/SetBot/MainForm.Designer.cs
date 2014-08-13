4aeb8b35160eb7e49d8fad6c243a49b35ea0ca0f
István Donkó <istvan.donko+github@gmail.com>
2014-08-13 11:56:03 +0200

    Area selection and image capturing done

---
+++ SetBot/SetBot/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SetBot
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.setAreaButton = new System.Windows.Forms.Button();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // setAreaButton
+            // 
+            this.setAreaButton.Location = new System.Drawing.Point(13, 13);
+            this.setAreaButton.Name = "setAreaButton";
+            this.setAreaButton.Size = new System.Drawing.Size(75, 23);
+            this.setAreaButton.TabIndex = 0;
+            this.setAreaButton.Text = "Set Area";
+            this.setAreaButton.UseVisualStyleBackColor = true;
+            this.setAreaButton.Click += new System.EventHandler(this.setAreaButton_Click);
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(107, 13);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(75, 23);
+            this.captureButton.TabIndex = 1;
+            this.captureButton.Text = "Capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 307);
+            this.Controls.Add(this.captureButton);
+            this.Controls.Add(this.setAreaButton);
+            this.Name = "MainForm";
+            this.Text = "SetBot";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button setAreaButton;
+        private System.Windows.Forms.Button captureButton;
+    }
+}
+
---
+++ SetBot/SetBot/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetBot
+{
+    public partial class MainForm : Form
+    {
+        public bool settingArea = false;
+
+        public Rectangle area = new Rectangle();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void setAreaButton_Click(object sender, EventArgs e)
+        {
+            if (!settingArea)
+            {
+                area.X = Cursor.Position.X;
+                area.Y = Cursor.Position.Y;
+
+                settingArea = true;
+            }
+
+            else
+            {
+                area.Width = Cursor.Position.X - area.X;
+                area.Height = Cursor.Position.Y - area.Y;
+            }
+        }
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(area.Width, area.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(area.X, area.Y, 0, 0, area.Size);
+
+            bmp.Save(@"C:\Users\isti\Desktop\test.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+}
